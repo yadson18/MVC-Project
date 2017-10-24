9411c90d43ee386499e60a9c6f16d38917f5ba9a
yadson18 <yadson18@hotmail.com>
2017-10-24 13:12:57 -0300

    Concluir script para criação de classes

--- arquivos-novas-funções/create.sh
+++
@@ -1,1 +0,0 @@
-/opt/firebird/bin/isql 'localhost:/BD/SRICASH.FDB' -i 'sqlcomand.sql' -o 'select.txt' -u SYSDBA -p masterkey -page 1000000
--- index.php
+++ index.php
@@ -2,6 +2,5 @@
 	require_once CONFIG . "Functions/global-functions.php";
 	require_once CONFIG . "Functions/config-functions.php";
 	require_once CLASSES . "AutoLoad/AutoLoad.php";
-
 	AutoLoad::loadClasses();
 	TemplateSystem::getInstance()->loadTemplate();
