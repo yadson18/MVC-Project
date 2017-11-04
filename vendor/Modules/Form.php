03428e8293e7656c108ded168da450a0f3d07440
yadson18 <yadson18@hotmail.com>
2017-11-04 00:03:29 -0300

    Melhorando método para criar inputs

--- vendor/Modules/Form.php
+++ vendor/Modules/Form.php
@@ -1,71 +1,76 @@
 <?php  
-	class Form{
-		private $avaliableAttributes = [
+    class Form{
+        private $avaliableAttributes = [
             "input" => [
-            	"class", "id", "name", "placeholder", 
-            	"type" => [
-            		"number", "date", "datetime", "time", "text", "radio", 
-            		"checkbox", "submit", "button", "color", "password", "textarea"
-            	]
+                "class", "id", "name", "placeholder", "required",
+                "type" => [
+                    "number", "date", "datetime", "time", "text", "radio", 
+                    "checkbox", "submit", "button", "color", "password", "textarea"
+                ]
             ],
             "button" => [
-            	"class", "id", "type" => ["submit", "button"]
+                "class", "id", "type" => ["submit", "button"]
             ]
         ];
 
         protected function getAvaliableAtts(string $tagName){
-        	if(isset($this->avaliableAttributes[$tagName])){
-        		return $this->avaliableAttributes[$tagName];
-        	}
+            if(isset($this->avaliableAttributes[$tagName])){
+                return $this->avaliableAttributes[$tagName];
+            }
         }
 
         protected function getTagAttributes(string $tagName, array $attributes = null, string $labelName = null){
-        	if(!empty($tagName)){
-	        	$avaliableAttrs = $this->getAvaliableAtts($tagName);
-	        	$tagAttributes = [];
+            if(!empty($tagName)){
+                $avaliableAttrs = $this->getAvaliableAtts($tagName);
+                $tagAttributes = [];
 
                 if(!empty($attributes)){
-    	            foreach($attributes as $attribute => $value){
-    	            	if(in_array($attribute, $avaliableAttrs)){
-                            $tagAttributes[$attribute] = $value;
-    	            	}
-                        else if(isset($avaliableAttrs[$attribute])){
-                            if(in_array($value, $avaliableAttrs[$attribute])){
+                    foreach($attributes as $attribute => $value){
+                        if(is_string($attribute)){
+                            if(in_array($attribute, $avaliableAttrs)){
                                 $tagAttributes[$attribute] = $value;
                             }
+                            else if(isset($avaliableAttrs[$attribute])){
+                                if(in_array($value, $avaliableAttrs[$attribute])){
+                                    $tagAttributes[$attribute] = $value;
+                                }
+                            }
                         }
-    	            }
+                        else{
+                            $tagAttributes[$value] = "";
+                        }
+                    }
                 }
 
-	            if(
-	            	$tagName === "input" ||
-	            	$tagName === "select" ||
-	            	$tagName === "button"
-	            ){
-		            if(!array_key_exists("class", $tagAttributes)){
-		            	$tagAttributes["class"] = "form-control ".strtolower(removeSpecialChars($labelName));
+                if(
+                    $tagName === "input" ||
+                    $tagName === "select" ||
+                    $tagName === "button"
+                ){
+                    if(!array_key_exists("class", $tagAttributes)){
+                        $tagAttributes["class"] = "form-control ".strtolower(removeSpecialChars($labelName));
                         if($tagName === "button"){
                             $tagAttributes["class"] .= " btn btn-default";
                         }
-		            }
-		            if($tagName === "input" || $tagName === "select"){
-			            if(!array_key_exists("name", $tagAttributes)){
-			            	$tagAttributes["name"] = strtolower(removeSpecialChars($labelName));
-			            }
-		            }
-	            }
+                    }
+                    if($tagName === "input" || $tagName === "select"){
+                        if(!array_key_exists("name", $tagAttributes)){
+                            $tagAttributes["name"] = strtolower(removeSpecialChars($labelName));
+                        }
+                    }
+                }
 
-	            $stringAttributes = "";
-	            foreach($tagAttributes as $attribute => $value){
-	            	$stringAttributes .= " {$attribute}='{$value}'";
-	            }
+                $stringAttributes = "";
+                foreach($tagAttributes as $attribute => $value){
+                    $stringAttributes .= " {$attribute}='{$value}'";
+                }
 
-	            return $stringAttributes;
-        	}
-        	return false;
+                return $stringAttributes;
+            }
+            return false;
         }
 
-		public function start(array $config){
+        public function start(array $config){
             $form = "<form";
 
             foreach ($config as $attribute => $value){
@@ -81,15 +86,15 @@
 
         public function input(string $name, array $attributes = null){
             if(!empty($name)){
-            	$inputAttributes = $this->getTagAttributes("input", $attributes, $name);
-            	if(!empty($inputAttributes)){
-                	return "
-		        		<div class='form-group'>
-		        			<label>{$name}</label>
-		        			<input {$inputAttributes}/>
-		        		</div>
-        			";
-            	}
+                $inputAttributes = $this->getTagAttributes("input", $attributes, $name);
+                if(!empty($inputAttributes)){
+                    return "
+                        <div class='form-group'>
+                            <label>{$name}</label>
+                            <input {$inputAttributes}/>
+                        </div>
+                    ";
+                }
             }
         }
 
@@ -105,4 +110,4 @@
                 }
             }
         }
-	}
+    }
